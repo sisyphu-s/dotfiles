0899428523fd0fa862fe6e2d3d33f634cfb519d2
sisyphu-s <121357821+sisyphu-s@users.noreply.github.com>
2024-07-06 13:27:31 +0700

    check-tools.sh

--- check-tools.sh
+++ check-tools.sh
@@ -58,4 +58,4 @@ for i in "${init[@]}"; do
 	fi
 done
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custome/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
