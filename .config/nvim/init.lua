ae6f7e262a393b1e81ad3de2533d114dcf3f6ba2
sisyphu-s <121357821+sisyphu-s@users.noreply.github.com>
2024-08-24 03:09:36 +0700

    init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,23 @@
 require("settings.keymaps")
 require("settings.options")
 require("lazy-setting.lazy")
+
+-- Assuming this Lua script is loaded when a PHP file is opened
+if vim.bo.filetype == "php" then
+	vim.b.coc_root_patterns = { ".git", ".env", "composer.json", "artisan" }
+end
+
+-- Set highlight for LineNr
+-- vim.cmd([[
+--   hi LineNr term=bold cterm=bold ctermfg=2 guifg=#0BA54E
+-- ]])
+
+vim.api.nvim_create_autocmd("BufRead", {
+	pattern = "*.blade.php",
+	command = "set filetype=php",
+})
+-- in my settings
+-- Set highlight for CursorLineNr
+vim.cmd([[
+  hi CursorLineNr guifg=#26C45C
+]])
